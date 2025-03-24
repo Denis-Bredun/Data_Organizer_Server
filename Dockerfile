0a85cd9a43f9057592965698959f4fa1f0efd164
Monkey D Luffy <bredun.denis@gmail.com>
2025-03-24 15:22:15 +0200

    Deleted unnecessary comments and optimized Program.cs

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
-# See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
-
-# This stage is used when running from VS in fast mode (Default for Debug configuration)
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
-# Add FFmpeg installation here (BEFORE setting the user)
+
 RUN apt-get update && apt-get install -y ffmpeg \
     && rm -rf /var/lib/apt/lists/*
     
@@ -11,7 +8,6 @@ WORKDIR /app
 EXPOSE 8080
 EXPOSE 8081
 
-# This stage is used to build the service project
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
@@ -21,12 +17,10 @@ COPY . .
 WORKDIR "/src/."
 RUN dotnet build "./Data_Organizer_Server.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
-# This stage is used to publish the service project to be copied to the final stage
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
 RUN dotnet publish "./Data_Organizer_Server.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
 
-# This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Data_Organizer_Server.Services;
 using FirebaseAdmin;
 using FirebaseAdminAuthentication.DependencyInjection.Extensions;
 using Google.Apis.Auth.OAuth2;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Data_Organizer_Server
 {
@@ -12,46 +13,58 @@ namespace Data_Organizer_Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            ConfigureLogging(builder);
+            ConfigureFirebase(builder);
+            ConfigureServices(builder.Services);
+
+            var app = builder.Build();
+
+            ConfigureMiddleware(app);
+
+            app.Logger.LogInformation("Starting the server...");
+            app.Run();
+        }
+
+        private static void ConfigureLogging(WebApplicationBuilder builder)
+        {
             builder.Logging.ClearProviders();
             builder.Logging.AddConsole();
             builder.Logging.AddDebug();
+        }
 
-            using var loggerFactory = LoggerFactory.Create(loggingBuilder =>
-            {
-                loggingBuilder.ClearProviders();
-                loggingBuilder.AddConsole();
-                loggingBuilder.AddDebug();
-            });
-
-            var logger = loggerFactory.CreateLogger<Program>();
-            logger.LogInformation("Starting initialization...");
-
+        private static void ConfigureFirebase(WebApplicationBuilder builder)
+        {
+            var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Initializing Firebase...");
+            
             var firebaseConfigJson = Environment.GetEnvironmentVariable("FIREBASE_CONFIG");
-            logger.LogInformation("Retrieved Firebase config from environment variable");
-
             if (string.IsNullOrEmpty(firebaseConfigJson))
             {
                 logger.LogCritical("FIREBASE_CONFIG is not set!");
                 throw new InvalidOperationException("Environment variable FIREBASE_CONFIG is not set!");
             }
 
-            logger.LogInformation("Initializing Firebase...");
             builder.Services.AddSingleton(FirebaseApp.Create(new AppOptions()
             {
                 Credential = GoogleCredential.FromJson(firebaseConfigJson)
             }));
-            logger.LogInformation("Firebase initialized");
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddAuthorization();
+            
             builder.Services.AddFirebaseAuthentication();
-            builder.Services.AddScoped<IOpenAIService, OpenAIService>();
-            builder.Services.AddScoped<IAzureService, AzureService>();
+            logger.LogInformation("Firebase initialized successfully");
+        }
 
-            var app = builder.Build();
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddAuthorization();
+            services.AddScoped<IOpenAIService, OpenAIService>();
+            services.AddScoped<IAzureService, AzureService>();
+        }
 
+        private static void ConfigureMiddleware(WebApplication app)
+        {
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -62,9 +75,6 @@ namespace Data_Organizer_Server
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
-
-            app.Logger.LogInformation("Starting the server...");
-            app.Run();
         }
     }
 }
