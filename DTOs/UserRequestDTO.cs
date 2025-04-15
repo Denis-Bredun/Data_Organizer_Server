76bd2181f5e282b63d9e17c3a3afc72924c0d373
Monkey D Luffy <bredun.denis@gmail.com>
2025-04-16 02:10:02 +0300

    Make User required and nullable others in UserRequestDTO with proper JSON property names.

--- DTOs/UserRequestDTO.cs
+++ DTOs/UserRequestDTO.cs
@@ -1,13 +1,25 @@
 ﻿using Data_Organizer_Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Data_Organizer_Server.DTOs
 {
     public class UserRequestDTO
     {
-        public User User { get; set; }
-        public UsersMetadata UsersMetadata { get; set; }
+        [Required]
+        [JsonPropertyName("user")]
+        public User User { get; set; } = default!;
+
+        [JsonPropertyName("usersMetadata")]
+        public UsersMetadata? UsersMetadata { get; set; }
+
+        [JsonPropertyName("creationDevice")]
         public DeviceInfoModel? CreationDevice { get; set; }
+
+        [JsonPropertyName("deletionDevice")]
         public DeviceInfoModel? DeletionDevice { get; set; }
-        public string Error { get; set; }
+
+        [JsonPropertyName("error")]
+        public string? Error { get; set; }
     }
 }
